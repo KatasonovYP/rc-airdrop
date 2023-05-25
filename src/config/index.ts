55fb47512fc6891ab120b12459a1f8dfd777e2ce
Yury Katasonov <katasonov-yury@yandex.ru>
2023-05-25 16:14:46 +0300

    add buttons

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,13 @@
 import { BrowserWalletConnector, ephemeralConnectorType, Network } from '@concordium/react-components';
 
 export const BROWSER_WALLET = ephemeralConnectorType(BrowserWalletConnector.create);
+
+export const MAX_CONTRACT_EXECUTION_ENERGY = BigInt(30_000);
+
+export const CONTRACT_NAME = 'airdrop_project'
+
+export const MODULE_REFERENCE  = 'bd0fafdd2384d23709f42ada1cd2f9f0ec2d262904adb2db4c1629f688961602'
+export const LP_RAW_SCHEMA  = '//8DAQAAAA8AAABhaXJkcm9wX3Byb2plY3QBABQABgAAAAkAAAB3aGl0ZWxpc3QQAgsJAAAAbmZ0X2xpbWl0BBUAAABuZnRfbGltaXRfcGVyX2FkZHJlc3MEDgAAAG5mdF90aW1lX2xpbWl0BAcAAAByZXNlcnZlBAgAAAB0b2tlbl9pZB0AAgAAAAkAAABjbGFpbV9uZnQEFAACAAAABQAAAHByb29mEAIWAgQAAABub2RlFgIVAwAAABAAAABQYXJzZVBhcmFtc0Vycm9yAg8AAABORlRMaW1pdFJlYWNoZWQCFQAAAEFkZHJlc3NOb3RPbldoaXRlbGlzdAIEAAAAdmlldwEUAAEAAAAYAAAAYW1vdW50X29mX2NsYWltZWRfdG9rZW5zBAA'
 export const testnet: Network = {
 	name: 'testnet',
 	genesisHash: '4221332d34e1694168c2a0c0b3fd0f273809612cb13d000d5c2e00e85f50f796',
---
+++ src/lib/transaction.ts
@@ -0,0 +1,45 @@
+import { AccountTransactionType, CcdAmount } from '@concordium/web-sdk';
+import { WalletConnection } from '@concordium/react-components';
+import { Info } from '../model';
+import { MAX_CONTRACT_EXECUTION_ENERGY } from '../config';
+
+function contractUpdatePayload(amount: CcdAmount, contract: Info, method: string) {
+	return {
+		amount,
+		address: {
+			index: contract.index,
+			subindex: BigInt(0),
+		},
+		receiveName: `${contract.name}.${method}`,
+		maxContractExecutionEnergy: MAX_CONTRACT_EXECUTION_ENERGY,
+	};
+}
+
+export async function initAirdrop(
+	connection: WalletConnection,
+	account: string, contract: Info,
+	whitelist: string[] = [],
+	nftLimit: number
+) {
+	return connection.signAndSendTransaction(
+		account,
+		AccountTransactionType.Update,
+		contractUpdatePayload(
+			new CcdAmount(BigInt(0)),
+			contract,
+			'Init'
+		),
+		{},
+		'',
+	);
+}
+
+export async function claimAirdrop(connection: WalletConnection, account: string, contract: Info) {
+	return connection.signAndSendTransaction(
+		account,
+		AccountTransactionType.Update,
+		contractUpdatePayload(new CcdAmount(BigInt(0)), contract, 'contract_claim_nft'),
+		{},
+		'',
+	);
+}
