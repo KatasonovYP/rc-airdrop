3604c30cf24602d3f5ea960b733365ab277a5541
andrea rettaroli <andrea.rettaroli@studio.unibo.it>
2023-05-26 21:54:34 +0200

    fix linker

--- src/components/styled-link/index.tsx
+++ src/components/styled-link/index.tsx
@@ -1,4 +1,5 @@
 import { FC, ReactNode } from 'react';
+import { Link } from "react-router-dom";
 
 interface Properties {
 	children: ReactNode;
@@ -8,8 +9,8 @@ interface Properties {
 
 export const StyledLink: FC<Properties> = ({ children, href, description }) => {
 	return (
-		<a
-			href={href}
+		<Link
+			to={href}
 			className='group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30'
 		>
 			<h2 className={`mb-3 text-2xl font-semibold`}>
@@ -19,6 +20,6 @@ export const StyledLink: FC<Properties> = ({ children, href, description }) => {
 				</span>
 			</h2>
 			<p className={`m-0 max-w-[30ch] text-sm opacity-50`}>{description}</p>
-		</a>
+		</Link>
 	);
 };
