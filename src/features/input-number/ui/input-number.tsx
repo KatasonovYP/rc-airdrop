07de7674b8905c38058f0eb4912d7e9d9db62925
Yury Katasonov <katasonov-yury@yandex.ru>
2023-05-31 23:20:30 +0300

    styling input-number

--- src/features/input-number/ui/input-number.tsx
+++ src/features/input-number/ui/input-number.tsx
@@ -1,12 +1,15 @@
 import { type FC } from 'react';
 import { type FieldErrors, type FieldValues, type Path, type UseFormRegister } from 'react-hook-form';
 import { TextAlert } from 'shared/components/text-alert';
+import cls from './input-number.module.css';
+import classNames from 'classnames';
 
 export const numberRegExp = /^\d+$/;
 // export const numberRegExp = /^([^0\D]\d+)|0$/;
 
 
 interface NumberInputProperties<T extends FieldValues> {
+	className?: string;
 	name: Path<T>;
 	register: UseFormRegister<T>;
 	errors: FieldErrors<T>;
@@ -22,17 +25,19 @@ export const InputNumber: customFC = <T extends FieldValues>(props: NumberInputP
 		register,
 		errors,
 		defaultValue,
+		className
 	} = props;
 	return (
-		<>
-			<div>{name}</div>
+		<div className={classNames(cls.inputBox, className)}>
 			<input
-				placeholder={`Введите ${name}`}
+				type='text'
+				required={true}
 				defaultValue={defaultValue}
 				{...register(name, { required: true, pattern: numberRegExp })}
 			/>
+			<span>{name}</span>
 			{errors[name]?.type === 'required' && <TextAlert>Field {name} is required</TextAlert>}
 			{errors[name]?.type === 'pattern' && <TextAlert>Input correct number</TextAlert>}
-		</>
+		</div>
 	);
 };
