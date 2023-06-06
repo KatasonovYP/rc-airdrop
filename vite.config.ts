251c5ddba4322a8bf761f985c3e7f5900f9e422d
Yury Katasonov <katasonov-yury@yandex.ru>
2023-06-06 09:56:49 +0300

    add merkle tree wasm

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,13 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import { visualizer } from 'rollup-plugin-visualizer';
+import wasm from 'vite-plugin-wasm';
 
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [
+		wasm(),
 		react(),
 		tsconfigPaths(),
 		visualizer({
