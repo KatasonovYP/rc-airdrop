4a396b163179462621d9c9814aa9f8e98a3375d3
Yury Katasonov <katasonov-yury@yandex.ru>
2023-06-07 22:06:20 +0300

    fix button width

--- src/shared/components/styled-button/ui/styled-button.tsx
+++ src/shared/components/styled-button/ui/styled-button.tsx
@@ -8,7 +8,7 @@ export const StyledButton: FC<Properties> = (props) => {
 	const { children, description } = props;
 	return (
 		<button
-			className='group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30'
+			className='max-w-fit group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30'
 			{...props}
 		>
 			<h2 className={`mb-3 text-2xl font-semibold`}>
